9ec2ef1ef99bd795d0081c2aa2cd3b070f2ee993
Abdelaziz Soliman <51516029+AbdelazizSoliman@users.noreply.github.com>
2021-07-03 09:12:26 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,106 +1,20 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
 const nav = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+const navigationMenu = () => {
 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-const navimplement = () => {
-
-    let navUlI = ``;
+    let navUI = ``;
     // looping all sections
     sections.forEach(section => {
 
         const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
+        const sectionNav = section.dataset.nav;
 
-        navUlI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+        navUlI += `<li><a class="menu__link" href="#${sectionID}">${sectionNav}</a></li>`;
 
     });
     // append all elements to the navigation
-    nav.innerHTML=navUlI;
+    nav.innerHTML=navUI;
 
 };
-navimplement();
-
-
-// Add class 'active' to section when near top of viewport
-const offset = (section) => {
-    return Math.floor(section.getBoundingClientRect().top);
-};
-
-// remove the active class
-const removeActive = (section) => {
-    section.classList.remove('your-active-class');
-    section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
-};
-// adding the active class
-const addActive = (conditional, section) => {
-    if(conditional){
-        section.classList.add('your-active-class');
-        section.style.cssText = "background-color: rgb(238, 91, 65)";
-    };
-};
-
-
-
-const toggleActiveState = () => {
-    sections.forEach(section => {
-        const elementOffset = offset(section);
-
-        inviewport = () => elementOffset < 150 && elementOffset >= -150;
-
-        removeActive(section);
-        addActive(inviewport(),section);
-    });
-};
-
-window.addEventListener('scroll' ,toggleActiveState);
-
-// Scroll to anchor ID using scrollTO event
-
-const scrollIntoView = () => {
-
-    const navlinks = document.querySelectorAll('.navbar__menu a');
-    navlinks.forEach(link => {
-        link.addEventListener('click', () => {
-            for(i = 0 ; i<sections ; i++){
-                sections[i].addEventListener("click",sectionScroll(link));
-            }
-        });
-    });
-
-};
-
-scrollIntoView();
+navigationMenu();
